f78209c298744bf3df2950b93dd8f82a7359768f
Ben Anderson <bentonanderson@outlook.com>
2022-08-09 17:32:53 -0500

    update

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,28 @@
 import numpy as np
 
 
+num_fas = {
+    'FA': 1,
+    'AC': 1,
+    'LysoPC': 1,
+    'LysoPE': 1,
+    'LysoPI': 1,
+    'CE': 1,
+    'Plasmenyl-PE': 2, 
+    'Plasmenyl-PC': 2,
+    'Plasmanyl-PC': 2,
+    'Plasmanyl-PE': 2,
+    'PE': 2,
+    'PC': 2,
+    'PI': 2,
+    'SM': 2,
+    'DG': 2,
+    'Alkenyl-DG': 2,
+    'Cer[NS]': 2,
+    'TG': 3,
+}
+
+
 def parse_p_value(pval):
     if   pval < 0.0001:
         return '****'
@@ -16,8 +38,16 @@ def parse_p_value(pval):
 
 def parse_lipid(lipid):
     """
-    Input lipid string e.g. "TG 60:4" or "PC 18:2_20:1"
-    Returns: tuple of (lipid class, SM label, # carbons, # unsat, sum composition string, FA list)
+    Input lipid string e.g. "TG 60:4", "PC 18:2_20:1", "SM d37:1"
+    Returns: tuple of (
+        lipid class, 
+        class label, 
+        # FA carbons, 
+        # FA unsat, 
+        sum composition string, 
+        FA list, 
+        is_sum_comp boolean
+    )
     """
     try:
         l = lipid.split(' ')
@@ -35,15 +65,22 @@ def parse_lipid(lipid):
             label = ''
 
         fa = [x.split(':') for x in fa.split('_')]
-        fa = [[int(x) for x in sublist] for sublist in fa]
+        fa = [tuple(int(x) for x in sublist) for sublist in fa]
         carbons = sum([chain[0] for chain in fa])
         unsat = sum([chain[1] for chain in fa])
         sum_comp = str(carbons) + ':' + str(unsat)
         result = cls + ' ' + label + str(carbons) + ':' + str(unsat)
-        return cls, label, carbons, unsat, sum_comp, fa
+        
+        is_sum_comp = False
+        if num_fas[cls] == 1:
+            is_sum_comp = False
+        elif len(fa) == 1:
+            is_sum_comp = True
+        
+        return cls, label, carbons, unsat, sum_comp, fa, is_sum_comp
     except:
-        # return a tuple of 6 nan
-        return (np.nan,) * 6
+        # return a tuple of nan
+        return (np.nan,) * 7
         
         
 def tight_bbox(ax):
@@ -51,12 +88,22 @@ def tight_bbox(ax):
     Example for placing figure panel letters at very top left of the axis bounding box: 
     for ax, letter in zip([ax1, ax2], ['A', 'B']):
         bb = tight_bbox(ax)
-        ax.text(x=bb.x0, y=bb.y1, s=letter, transform=fig.transFigure)
+        ax.text(x=bb.x0, y=bb.y1, s=letter, transform=fig.transFigure, fontweight='bold')
     """
-
     fig = ax.get_figure()
     tight_bbox_raw = ax.get_tightbbox(fig.canvas.get_renderer())
     from matplotlib.transforms import TransformedBbox
     tight_bbox_fig = TransformedBbox(tight_bbox_raw, fig.transFigure.inverted())
     return tight_bbox_fig
     
+    
+def format_number(num):
+    """
+    From https://stackoverflow.com/a/45846841/8659119
+    """
+    num = float('{:.3g}'.format(num))
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
